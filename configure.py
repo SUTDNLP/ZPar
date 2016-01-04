282a7d42586e50a280a22d2fcd2c12d7c5daea28
Jiangming Liu <jmliunlp@gmail.com>
2016-01-04 15:08:49 +0800

    add generic and spanish

--- configure.py
+++ configure.py
@@ -2,54 +2,82 @@
 
 import sys
 
+#===================================
+# English application
+#===================================
 #english postagger: collins(default)
 ENGLISH_TAGGER = "collins"
 
-#english constituency parser: jiangming(default), muhua, cad, cad-mvt, lin, miguel, srnew
-ENGLISH_CONSTITUENCY_PARSER = "jiangming"
+#english constituency parser: jiangming, muhua(default), cad, cad-mvt, lin, miguel, srnew
+ENGLISH_CONSTITUENCY_PARSER = "muhuan"
 
 #english dependency parser: arceager(default), arcstandard, choi, choibasic
-#                           emnlp08, liu, morphparser, punct, uppsala
+# 							emnlp08, liu, morphparser, punct, uppsala
 ENGLISH_DEPENDENCY_PARSER = "arceager"
 
-#english character-based dependency parser: arceager, arcstandard, arcstandard-loc
-#                                           arcstandardfilter, arcstandbuffer
+#english character-based dependency parser:	arceager(default), arcstandard, arcstandard-loc
+#											arcstandardfilter
 ENGLISH_POSDEPENDENCY_PARSER = "arceager"
 
 #english dependency labeler: naive(default)
 ENGLISH_DEPENDENCY_LABELER = "naive"
 
+#english constituency to stanford arc: jiangming_convert(default)
 ENGLISH_STANFORD_CONVERTER = "jiangming_convert"
+
+
+#===================================
+# Chinese application
 #===================================
 #chinese postagger: agenda(default), agenda_mvt,segmented
 CHINESE_TAGGER = "agenda"
 
-#chinese segmentor: agenda(default),
+#chinese segmentor: agenda(default)
 CHINESE_SEGMENTOR = "agenda"
 
 #chinese constituency parser: acl13(default), jcad
-CHINESE_CONSTITUENCY_PARSER  = "acl13"
+CHINESE_CONSTITUENCY_PARSER = "acl13"
 
 #chinese dependency parser: arceager(default), arceager-mvt, arceager-mvt-origin
-#                           arcstandard, choi, choibasic, emnlp08, liu, uppsala
-CHINESE_DEPENDENCY_PARSER  = "arceager"
+#							arcstandard, choi, choibasic, emnlp08, liu, uppsala
+CHINESE_DEPENDENCY_PARSER = "arceager"
 
-#chinese character-based dependency parser: jcad, jcadeager
-#
-CHINESE_POSDEPENDENCY_PARSER = "jcad"
+#chinese character-based dependency parser:	jcad, jcadeager
+CHINESE_POSDEPENDENCY_PARSER = "jcadeager"
 
 #chinese dependency labeler: naive(default)
 CHINESE_DEPENDENCY_LABELER = "naive"
 
+#===================================
+# Generic applicatin
+#===================================
+#generic postagger: collins(default)
 GENERIC_TAGGER = "collins"
 
-GENERIC_CONSTITUENCY_PARSER = "muhua"
+#generic constituency parser: srnew(default)
+GENERIC_CONSTITUENCY_PARSER = "srnew"
 
+#generic dependency parser: arceager(default)
 GENERIC_DEPENDENCY_PARSER = "arceager"
 
+#generic dependency labeler: naive(default)
 GENERIC_DEPENDENCY_LABELER = "naive"
 
+#generic ccg parser: srnew(default)
 GENERIC_CCG_PARSER = "srnew"
+
+#===================================
+# Spanish application
+#===================================
+#spanish postagger: collins(default)
+SPANISH_TAGGER = "collins"
+
+#spanish dependency parser: arceager(default)
+SPANISH_DEPENDENCY_PARSER = "arceager"
+
+#spanish depedndency labeler: naive(default)
+SPANISH_DEPENDENCY_LABELER = "naive"
+
 def write_common_thing(cout):
 	cout.write("${SOURCE_DIR}/libs/reader.cpp\n")
 	cout.write("${SOURCE_DIR}/libs/writer.cpp\n")
@@ -65,13 +93,34 @@ def add_libs(cout, name, libs):
 			cout.write(lib+"\n")
 		cout.write(")\n")
 
-if len(sys.argv) <= 2:
+def check(l):
+	if len(l) != 2:
+		return False
+	if l[0] == "english":
+		if not l[1] in ["postagger", "conparser", "depparser", "deplabeler", "posdepparser", "converter"]:
+			return False
+	if l[0] == "chinese":
+		if not l[1] in ["segmentor", "postagger", "conparser", "depparser", "deplabeler", "posdepparser"]:
+			return False
+	if l[0] == "generic":
+		if not l[1] in ["ccgparser", "postagger", "conparser", "depparser", "deplabeler"]:
+			return False
+	if l[0] == "spanish":
+		if not l[1] in ["postagger", "depparser", "deplabeler"]:
+			return False
+	return True
+
+if check(sys.argv[1:]) == False:
 	sys.stderr.write("./configure.py [language] [app]\n")
 	sys.stderr.write("\t[language]\n")
 	sys.stderr.write("\t\tenglish [app]\n")
-	sys.stderr.write("\t\t\tpostagger|conparser|depparser|deplabeler\n")
+	sys.stderr.write("\t\t\tpostagger|conparser|depparser|deplabeler|posdepparser|converter\n")
 	sys.stderr.write("\t\tchinese [app]\n")
-	sys.stderr.write("\t\t\tpostagger|conparser|depparser|deplabeler\n")
+	sys.stderr.write("\t\t\tsegmentor|postagger|conparser|depparser|deplabeler|posdepparser\n")
+	sys.stderr.write("\t\tgeneric [app]\n")
+	sys.stderr.write("\t\t\tccgparser|postagger|conparser|depparser|deplabeler\n")
+	sys.stderr.write("\t\tspanish [app]\n")
+	sys.stderr.write("\t\t\tpostagger|depparser|deplabeler\n")
 	sys.stderr.write("example: ./configure.py english postagger\n")
 	exit(1)
 out = open("CMakeLists.txt","w")
@@ -125,6 +174,12 @@ for line in open("setting"):
 		GENERIC_DEPENDENCY_LABELER = line[1]
 	elif line[0] == "GENERIC_CCG_PARSER":
 		GENERIC_CCG_PARSER = line[1]
+	elif line[0] == "SPANISH_TAGGER":
+		SPANISH_TAGGER = line[1]
+	elif line[0] == "SPANISH_DEPENDENCY_PARSER":
+		SPANISH_DEPENDENCY_PARSER = line[1]
+	elif line[0] == "SPANISH_DEPENDENCY_LABELER":
+		SPANISH_DEPENDENCY_LABELER = line[1]
 
 out.write("include_directories (${SOURCE_DIR} ${SOURCE_DIR}/include)\n")
 out.write("include_directories (${SOURCE_DIR}/"+sys.argv[1]+")\n")
@@ -402,6 +457,14 @@ elif sys.argv[1] == "chinese":
 		out.write("${SOURCE_DIR}/common/deplabeler/main.cpp\n")
 		out.write(")\n")
 		add_libs(out,"chinese.deplabeler.train",libs)
+	elif sys.argv[2] == "doc2snt":
+		out.write('set (CMAKE_CXX_FLAGS "-std=c++11 -g -w -W -O3 -DNDEBUG")\n')
+		
+		out.write("add_executable (chinese.doc2snt\n")
+		write_common_thing(out)
+		out.write("${SOURCE_DIR}/chinese/doc2snt/doc2snt.cpp\n")
+		out.write("${SOURCE_DIR}/chinese/doc2snt/main.cpp\n")
+		out.write(")\n")
 
 elif sys.argv[1] == "generic":
 	if sys.argv[2] == "postagger":
@@ -531,3 +594,67 @@ elif sys.argv[1] == "generic":
 		out.write("${SOURCE_DIR}/common/conparser/train.cpp\n")
 		out.write(")\n")
 		add_libs(out,"generic.ccgparser.train",libs)
+elif sys.argv[1] == "spanish":
+	if sys.argv[2] == "postagger":
+		out.write('set (CMAKE_CXX_FLAGS "-std=c++11 -g -w -W -O3 -DNDEBUG -DES09_DEPENDENCIES -DTARGET_LANGUAGE='+sys.argv[1]+'")\n')
+		out.write("include_directories (${SOURCE_DIR}/common/tagger)\n")
+		out.write("include_directories (${SOURCE_DIR}/common/tagger/implementations/"+SPANISH_TAGGER+")\n")
+
+		out.write("add_executable (spanish.postagger.tagger\n")
+		write_common_thing(out)
+		out.write("${SOURCE_DIR}/common/tagger/implementations/"+SPANISH_TAGGER+"/tagger.cpp\n")
+		out.write("${SOURCE_DIR}/common/tagger/implementations/"+SPANISH_TAGGER+"/tagger_weight.cpp\n")
+		out.write("${SOURCE_DIR}/common/tagger/main.cpp\n")
+		out.write(")\n")
+		add_libs(out,"spanish.postagger.tagger",libs)
+
+		out.write("add_executable (spanish.postagger.train\n")
+		write_common_thing(out)
+		out.write("${SOURCE_DIR}/common/tagger/implementations/"+SPANISH_TAGGER+"/tagger.cpp\n")
+		out.write("${SOURCE_DIR}/common/tagger/implementations/"+SPANISH_TAGGER+"/tagger_weight.cpp\n")
+		out.write("${SOURCE_DIR}/common/tagger/train.cpp\n")
+		out.write(")\n")
+		add_libs(out,"spanish.postagger.train",libs)
+
+	elif sys.argv[2] == "depparser":
+		out.write('set (CMAKE_CXX_FLAGS "-std=c++11 -g -w -W -O3 -DNDEBUG -DES09_DEPENDENCIES -DLABELED -DTARGET_LANGUAGE='+sys.argv[1]+'")\n')
+		out.write("include_directories (${SOURCE_DIR}/common/depparser)\n")
+		out.write("include_directories (${SOURCE_DIR}/common/depparser/implementations/"+SPANISH_DEPENDENCY_PARSER+")\n")
+
+		out.write("add_executable (spanish.depparser.parser\n")
+		write_common_thing(out)
+		out.write("${SOURCE_DIR}/common/depparser/implementations/"+SPANISH_DEPENDENCY_PARSER+"/depparser.cpp\n")
+		out.write("${SOURCE_DIR}/common/depparser/implementations/"+SPANISH_DEPENDENCY_PARSER+"/depparser_weight.cpp\n")
+		out.write("${SOURCE_DIR}/common/depparser/main.cpp\n")
+		out.write(")\n")
+		add_libs(out,"spanish.depparser.parser",libs)
+
+		out.write('set (CMAKE_CXX_FLAGS "-std=c++11 -g -w -W -O3 -DNDEBUG -DES09_DEPENDENCIES -DLABELED -DTARGET_LANGUAGE='+sys.argv[1]+'")\n')
+		out.write("add_executable (spanish.depparser.train\n")
+		write_common_thing(out)
+		out.write("${SOURCE_DIR}/common/depparser/implementations/"+SPANISH_DEPENDENCY_PARSER+"/depparser.cpp\n")
+		out.write("${SOURCE_DIR}/common/depparser/implementations/"+SPANISH_DEPENDENCY_PARSER+"/depparser_weight.cpp\n")
+		out.write("${SOURCE_DIR}/common/depparser/train.cpp\n")
+		out.write(")\n")
+		add_libs(out,"spanish.depparser.train",libs)
+	elif sys.argv[2] == "deplabeler":
+		out.write('set (CMAKE_CXX_FLAGS "-std=c++11 -g -w -W -O3 -DNDEBUG -DES09_DEPENDENCIES -DTARGET_LANGUAGE='+sys.argv[1]+'")\n')
+		out.write("include_directories (${SOURCE_DIR}/common/deplabeler)\n")
+		out.write("include_directories (${SOURCE_DIR}/common/deplabeler/implementations/"+SPANISH_DEPENDENCY_LABELER+")\n")
+
+		out.write("add_executable (spanish.deplabeler.labeler\n")
+		write_common_thing(out)
+		out.write("${SOURCE_DIR}/common/deplabeler/implementations/"+SPANISH_DEPENDENCY_LABELER+"/deplabeler.cpp\n")
+		out.write("${SOURCE_DIR}/common/deplabeler/implementations/"+SPANISH_DEPENDENCY_LABELER+"/weight.cpp\n")
+		out.write("${SOURCE_DIR}/common/deplabeler/main.cpp\n")
+		out.write(")\n")
+		add_libs(out,"spanish.deplabeler.labeler",libs)
+
+		out.write("add_executable (spanish.deplabeler.train\n")
+		write_common_thing(out)
+		out.write("${SOURCE_DIR}/common/deplabeler/implementations/"+SPANISH_DEPENDENCY_LABELER+"/deplabeler.cpp\n")
+		out.write("${SOURCE_DIR}/common/deplabeler/implementations/"+SPANISH_DEPENDENCY_LABELER+"/weight.cpp\n")
+		out.write("${SOURCE_DIR}/common/deplabeler/main.cpp\n")
+		out.write(")\n")
+		add_libs(out,"spanish.deplabeler.train",libs)
+	
